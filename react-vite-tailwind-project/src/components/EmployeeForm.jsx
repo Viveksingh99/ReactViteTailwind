04644116c29bc13ed0b34c0389bb6aadfe87ebd7
viveksingh99 <vivek22jan99@gmail.com>
2023-09-13 00:30:30 +0530

    second commit

---
+++ react-vite-tailwind-project/src/components/EmployeeForm.jsx
@@ -0,0 +1,42 @@
+function EmployeeForm(){
+    return(
+        <>
+        <div>
+            <h2 className="text-center">
+            Employee  Registration  Form
+            </h2>
+        </div>
+        <div className="h-[1624 px] w-[1053 px] flex justify-center">   
+            <div className="flex">
+            <div>
+                <p>First Name*</p>
+                <input placeholder="Enter your name" />
+            </div>
+            <div>
+                <p>Last Name*</p>
+                <input placeholder="Enter your name" />
+            </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+        <div className="h-[1624 px] w-[1053 px] flex justify-center">   
+            <div className="flex">
+            <div>
+                <p>First Name*</p>
+                <input placeholder="Enter your name" />
+            </div>
+            <div>
+                <p>Last Name*</p>
+                <input placeholder="Enter your name" />
+            </div>
+            </div>
+            <div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default EmployeeForm;
